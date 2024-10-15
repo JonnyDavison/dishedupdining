696016273624e126df9b165d6a520453e777d4a1
JonnyDavison <jonnydavison@icloud.com>
2024-10-15 19:32:48 +0000

    Update settings & custom storages

--- core/settings.py
+++ core/settings.py
@@ -216,7 +216,7 @@ if 'USE_AWS' in os.environ:
     }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'dishedupdining'
-    AWS_S3_REGION_NAME = 'eu-west-1'  # Make sure this matches your bucket's region
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
