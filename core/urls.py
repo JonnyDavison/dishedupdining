d1bc0d2b062a0d8ba6ddc8fe858e477a50d79cba
JonnyDavison <jonnydavison@icloud.com>
2024-10-15 21:50:31 +0000

    Update sitemap.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import sitemaps  # Import the sitemaps dictionary
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +13,6 @@ urlpatterns = [
     path('', include('index.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('profiles/', include('profiles.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
