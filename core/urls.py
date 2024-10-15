db793d4701702c4033c2c30eed04b3807a0863f8
JonnyDavison <jonnydavison@icloud.com>
2024-10-15 22:07:13 +0000

    Add sitemap.xml

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.sitemaps.views import sitemap
-from .sitemaps import sitemaps  # Import the sitemaps dictionary
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +11,4 @@ urlpatterns = [
     path('', include('index.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('profiles/', include('profiles.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
