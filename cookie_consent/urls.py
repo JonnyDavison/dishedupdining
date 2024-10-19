79794c3962750def8e8c7d805f67890c576abaf0
JonnyDavison <jonnydavison@icloud.com>
2024-10-19 14:51:43 +0000

    Update cookicies/privacy policy html

--- cookie_consent/urls.py
+++ cookie_consent/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import policy_view
 
 urlpatterns = [
     path('set-cookie-consent/', views.set_cookie_consent, name='set_cookie_consent'),
+    path('<str:policy_type>/', policy_view, name='policy_view'),
 ]
--- cookie_consent/views.py
+++ cookie_consent/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .models import CookieConsent
+from django.shortcuts import render
+from index.models import Home
 
 @require_POST
 def set_cookie_consent(request):
@@ -20,3 +22,23 @@ def set_cookie_consent(request):
     request.session['cookie_consent_analytics'] = consent.analytics
 
     return JsonResponse({'status': 'success'})
+
+
+def policy_view(request, policy_type):
+    home = Home.objects.filter(is_active=True).first()
+    if policy_type == 'cookie':
+        template_name = 'cookie_consent/cookie-policy.html'
+        title = 'Cookie Policy'
+    elif policy_type == 'privacy':
+        template_name = 'cookie_consent/privacy-policy.html'
+        title = 'Privacy Policy'
+    else:
+        # Handle invalid policy type
+        return render(request, '404.html')  # Or any other error handling
+
+    context = {
+        'home': home,
+        'title': title,
+        }
+
+    return render(request, template_name, context)
