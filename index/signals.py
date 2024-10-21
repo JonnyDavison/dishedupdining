d21eb7554d3a046c11eaa7b5d08751f69cfad028
JonnyDavison <jonnydavison@icloud.com>
2024-10-21 08:37:24 +0000

    Update signals

--- index/signals.py
+++ index/signals.py
@@ -5,14 +5,15 @@ from django.apps import apps
 
 @receiver(post_save)
 def upload_to_s3(sender, instance, created, **kwargs):
-    # Check if the model has any FileField or ImageField
     file_fields = [f for f in instance._meta.fields if f.get_internal_type() in ['FileField', 'ImageField']]
     
     for field in file_fields:
         file = getattr(instance, field.name)
         if file:
             file_name = file.name
-            # Use default_storage to handle the file upload
-            with file.open() as f:
-                default_storage.save(file_name, f)
-            print(f"File {file_name} uploaded to S3")
+            if not default_storage.exists(file_name):
+                with file.open('rb') as f:
+                    default_storage.save(file_name, f)
+                print(f"File {file_name} uploaded to S3")
+            else:
+                print(f"File {file_name} already exists in S3")
