6c38bbb847b80927528482984ff298debffca3e0
JonnyDavison <jonnydavison@icloud.com>
2024-10-24 10:16:43 +0000

    Update the 404 views/context to display logo

--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from index.models import Home
 
 
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
-    return render(request, "errors/404.html", status=404)
+    home = Home.objects.filter(is_active=True).first()
+    context = {
+        'home': home,
+    }
+    return render(request, "errors/404.html", context, status=404)
                 
